da3d98aad4ec6bd964f1ba03d715bd34b8abff66
Pablo1313 <Pablo1313@PCPabloL>
2024-01-10 10:39:36 +0100

    segundo commit, añadiendo P1.java

---
+++ P4/src/main/java/com/mycompany/p4/P4.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.p4;
+
+/**
+ *
+ * @author Pablo1313
+ */
+public class P4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+                System.out.println("adios World!");
+
+    }
+}
